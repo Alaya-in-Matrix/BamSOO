241debda6c552fc4c4d04116ac0afc5ff3718c86
wenlong lyu <lvwenlong_lambda@qq.com>
2018-12-15 15:51:08 +0800

    Initial guess of GP hyperparameters

--- GP.py
+++ GP.py
@@ -21,19 +21,19 @@ class GP:
         kern   = GPy.kern.Matern52(input_dim = self.dim, ARD = True)
         self.m = GPy.models.GPRegression(self.train_x, self.train_y, kern)
 
-        
-        self.m.kern.variance       = np.var(self.train_y)
-        self.m.kern.lengthscale    = np.std(self.train_x, 0)
+        # self.m.kern.variance       = 64
+        # self.m.kern.lengthscale    = 3 * np.ones(self.dim)
+        # self.m.likelihood.variance = np.maximum(2e-20, 1e-2 * np.var(self.train_y))
+        # self.m.likelihood.variance.constrain_bounded(1e-20, 1e10)
+
+        self.m.kern.variance       = (self.train_y.max() - self.train_y.min())**2
+        self.m.kern.lengthscale    = 0.3 * (self.train_x.max(axis=0)  - self.train_x.min(axis=0))
         self.m.likelihood.variance = np.maximum(2e-20, 1e-2 * np.var(self.train_y))
         self.m.likelihood.variance.constrain_bounded(1e-20, 1e10)
 
-        # # self.m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(np.var(self.train_y), 120))
-        # # self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(1e-2 * np.var(self.train_y), 4))
-        # # self.m.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(np.std(self.train_x, 0), 1000 * np.ones(np.std(self.train_x, 0).shape)))
-
-        # self.m.kern.variance.set_prior(GPy.priors.LogGaussian(0, 1))
-        # self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(0.02, 4))
-        # self.m.kern.lengthscale.set_prior(GPy.priors.LogGaussian(0, 10))
+        self.m.kern.variance.set_prior(GPy.priors.LogGaussian(np.log(self.m.kern.variance), 1))
+        self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(0.02, 4))
+        self.m.kern.lengthscale.set_prior(GPy.priors.LogGaussian(0, 10))
 
     def train(self):
         self.m.optimize(max_iters=100, messages=False)
